93c82754821940b9be09fb591ddc273669e81447
Sascha Brägger <sascha.braegger-android@hotmail.com>
2019-09-06 11:14:18 +0200

    changed plus and minus button to be on side of nummber

--- app/src/main/java/com/example/interactive2/MainActivity.java
+++ app/src/main/java/com/example/interactive2/MainActivity.java
@@ -59,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        /**priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));**/
+        priceTextView.setText("CHF "+number);
     }
 
 }
